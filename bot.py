3bc7e8ba9a64f3afb946e5a18c661bbab7d3b433
Tchakatak <maxime.cheminade@systechno.fr>
2018-11-21 21:25:01 +0100

    Initial commit

---
+++ bot.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+
+from irc import *
+import time
+import re
+import base64
+import string
+
+
+def init(server, channel, nickname, testchannel):
+    irc.connect(server)
+    irc.nickuser(nickname)
+    time.sleep(2)
+    irc.channel(testchannel)
+    irc.send(chan=testchannel, msg='Startup completed')
+
+def refresh():
+    text = irc.get_text()
+    print(text)
+    return text
+
+
+def eq1():
+    irc.send(chan=channel, msg='!ep1')
+    text=refresh()
+    m = re.search('([0-9]*)\s/\s([0-9]*)', str(text))
+    if m is not None:
+        print('[+] ' + str(m))
+        print('[+] Match numbers ' + m.group(0))
+        num1 = float(m.group(1))
+        print(num1)
+        num2 = float(m.group(2))
+        print(num2)
+        num3 = calc(num1, num2)
+        print(num3)
+        forge = '!ep1 -rep ' + str(num3)
+        irc.send(chan=testchannel, msg=forge)
+        irc.send(chan=channel, msg=forge)
+
+
+def eq2():
+    irc.send(chan=channel, msg='!ep2')
+    text=refresh()
+    m = re.search('(?<= botep \:)(.*)', str(text))
+    print(m)
+    if m is not None:
+        try :
+            print('[+] catched ' + str(text))
+            decode = base64.b64decode(str(m.group(1)))
+            print('[+] Decoded string ' + str(decode) + ' From ' + str(m.group(1)) )
+            forge = '!ep2 -rep ' + str(decode)
+            print('[+] Sending back ' + str(forge))
+            irc.send(chan=testchannel, msg=forge)
+            irc.send(chan=channel, msg=forge)
+            time.sleep(5)
+        except :
+            time.sleep(1)
+
+def eq3():
+    irc.send(chan=channel, msg='!ep3')
+    text = refresh()
+    m = re.search('(?<= botep \:)(.*)', str(text))
+    print(m)
+    if m is not None:
+        try:
+            rot13 = string.maketrans(
+            "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz",
+            "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm")
+            decode = string.translate(str(m.group(1)), rot13)
+            forge = '!ep3 -rep ' + str(decode)
+            print('[+] Sending back ' + str(forge))
+            irc.send(chan=testchannel, msg=forge)
+            irc.send(chan=channel, msg=forge)
+            time.sleep(5)
+        except:
+            time.sleep(1)
+
+def eq4():
+    time.sleep(1)
+
+
+
+irc = IRC()
+channel = "candy"
+testchannel = "#testit"
+server = "irc.root-me.org"
+nickname = "botep"
+init(server, channel, nickname, testchannel)
+
+
+while 1:
+    text = refresh()
+    if text.find('PINGtest') != -1:
+        irc.send(chan=testchannel, msg='pong')
+    if text.find('eq1start') != -1:
+        eq1()
+    if text.find('eq2start') != -1:
+        eq2()
+    if text.find('eq3start') != -1:
+        eq3()
+    if text.find('eq4start') != -1:
+        eq4()
---
+++ irc.py
@@ -0,0 +1,30 @@
+import socket
+
+class IRC:
+    irc = socket.socket()
+
+    def __init__(self):
+        self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def send(self, chan, msg):
+        self.irc.send("PRIVMSG " + chan + " " + msg + "\r\n")
+
+    def connect(self, server):
+        # defines the socket
+        print("connecting to:" + server)
+        self.irc.connect((server, 6667))  # connects to the server
+
+    def nickuser(self, nickname):
+        self.irc.send("USER " + nickname + " " + nickname + " " + nickname + " bot!\r\n")  # user authentication
+        print("[+] Send USER passed ")
+        self.irc.send("NICK " + nickname + "\r\n")
+        print("[+] Send NICK passed")
+
+    def channel(self, channel):
+        self.irc.send("JOIN " + channel + "\r\n")  # join the chan
+        print("[+] Send JOIN " + str(channel))
+
+    def get_text(self):
+        text = self.irc.recv(2040)  # receive the text
+        return text
+
