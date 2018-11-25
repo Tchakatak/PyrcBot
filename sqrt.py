edbd34e4ed8a478dd0845f071c42a2a0d8d94ead
Tchakatak <maxime.cheminade@systechno.fr>
2018-11-25 11:16:44 +0100

    Adding sqrt file for calc function in eq1

---
+++ sqrt.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import math
+
+def calc(num1, num2):
+    num1 = math.sqrt(float(num1))
+    print(float(num1))
+    num3 = float(num1) * float(num2)
+    num3 = round(float(num3), 2)
+    return num3
